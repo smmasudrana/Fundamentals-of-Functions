e0b3d14656484e5f5d2b4dfbeb826a39726e3b0b
smmasudrana <smmasudrana62@gmail.com>
2025-08-08 16:23:20 +0600

    add how to declare parameter

---
+++ parameter.js
@@ -0,0 +1,38 @@
+function vatKhao(){
+    console.log('at first wash your hand');
+    console.log('say bismillah before you start');
+    console.log('start with right side of plate');
+    console.log('when you finished say alhamdulillah')
+}
+
+vatKhao();
+
+//parameter declare
+function square (number){
+    console.log(number);
+    console.log('value of the number parameter', number)
+    const borgo= number*number;
+    console.log('square of the given number is:', borgo);
+}
+
+square(4);
+console.log('---------------------')
+square(5);
+square(12);
+square(50);
+square(405);
+
+
+
+//parameter declare /here is parameter is num1 and num2 value is 5 5
+function add (num1, num2){
+    const sum= num1 + num2;
+    console.log(sum);
+}
+
+add(5, 5);
+add(10, 50);
+add(62, 64);
+
+
+
