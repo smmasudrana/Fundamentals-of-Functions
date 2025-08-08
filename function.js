e103f661e27cb886545ab8ae91025390e951c4f8
smmasudrana <smmasudrana62@gmail.com>
2025-08-08 15:53:08 +0600

    add how to declare function

---
+++ function.js
@@ -0,0 +1,17 @@
+const price = 45;
+
+function fanOffKor (){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('clilck the swtich to off of the fan')
+}
+
+fanOffKor();
+
+function brushTeeth(){
+    console.log('pick up the brush')
+    console.log('add some paste')
+    console.log('at least brush your teeth 2 munites.')
+}
+
+brushTeeth();
